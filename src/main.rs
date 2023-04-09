b501a61248cba9dcb934bea1d6ed004cd28ae978
Taufik Mulyana <17433202+nothinux@users.noreply.github.com>
2023-04-09 23:53:24 +0700

    feat: add ability to specify resource name and filter based on labels
    * feat: add ability to specify resource name and filter based on labels

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
+use std::str::FromStr;
+
+use bpaf::Bpaf;
 use kube::{Client};
 use tabled::{Table, Style};
 
-#[cfg(test)]
-mod utils_test;
+#[derive(Clone, Debug, Bpaf)]
+#[bpaf(options, version)]
+/// a tool that provide kubernetes cluster resource information, including cpu, memory, storage and number of pods.
+struct Options {
+    #[bpaf(short('l'), long)]
+    /// filter spesific node using it's label
+    selector: Option<String>,
+    #[bpaf(short('t'), long("type"))]
+    /// filter based on resource type (eg: node, namespace), default: node
+    resource_type: Option<String>,
+}
+
 mod utils;
 mod kubernetes;
 
+#[cfg(test)]
+mod utils_test;
+
 #[tokio::main]
 async fn main() {
+    let opts = options().run();
+    let mut resource_type: kubernetes::ResourceType = kubernetes::ResourceType::Node;
+
+    if let Some(rt) = opts.resource_type {
+        resource_type =  kubernetes::ResourceType::from_str(&rt).unwrap();
+    }
+
     let client = match Client::try_default().await {
         Err(e) => {
             eprintln!("Error creating kubernetes client {:?}", e);
@@ -18,7 +41,7 @@ async fn main() {
 
     let mut resource_req = Vec::new();
 
-    kubernetes::collect_node_info(client.clone(), &mut resource_req).await;
+    kubernetes::collect_info(client.clone(), &mut resource_req, resource_type, opts.selector).await;
 
     let data = utils::parse_resource_data(&resource_req);
     let mut table = Table::new(&data);
